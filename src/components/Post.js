2634b307dc724039112ef184ffac1f7c57c2da3f
Nathan4Andrade <nathanandrade20@gmail.com>
2023-05-05 16:46:53 -0300

    impl posts

---
+++ src/components/Post.js
@@ -0,0 +1,43 @@
+export default function Post(props) {
+  return (
+    <div class="post">
+      <div class="topo">
+        <div class="usuario">
+          <img src={props.profilePicture} alt={props.altProfile} />
+          {props.username}
+        </div>
+        <div class="acoes">
+          <ion-icon name="ellipsis-horizontal"></ion-icon>
+        </div>
+      </div>
+
+      <div class="conteudo">
+        <img src={props.media} alt={props.mediaAlt} />
+      </div>
+
+      <div class="fundo">
+        <div class="acoes">
+          <div>
+            <ion-icon name="heart-outline"></ion-icon>
+            <ion-icon name="chatbubble-outline"></ion-icon>
+            <ion-icon name="paper-plane-outline"></ion-icon>
+          </div>
+          <div>
+            <ion-icon name="bookmark-outline"></ion-icon>
+          </div>
+        </div>
+
+        <div class="curtidas">
+          <img src="assets/eu.jpg" alt="respondeai" />
+          <div class="texto">
+            Curtido por <strong>nathanandrade</strong> e{" "}
+            <strong>
+              outras {Math.floor(Math.random() * 100) + 100}.
+              {Math.floor(Math.random() * 100) + 100} pessoas
+            </strong>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
